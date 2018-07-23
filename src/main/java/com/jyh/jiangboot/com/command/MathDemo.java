5e0ffd18ecb37a1949e7bab4dceedaf2b120ff3f
jiangyuhong <jyh.mm@qq.com>
2018-07-23 15:33:43 +0800

    数字雨Demo

---
+++ src/main/java/com/jyh/jiangboot/com/command/MathDemo.java
@@ -0,0 +1,25 @@
+package com.jyh.jiangboot.com.command;
+
+public class MathDemo {
+
+    
+
+    public static void main(String[] args) {
+        float a = (float) 300.0;
+
+        Integer b =Math.round(a);
+        System.out.println(a);
+        System.out.println(b);
+
+
+        int c = 5;
+        short d ;
+        d = (short) c;
+
+
+
+    }
+
+
+
+}
