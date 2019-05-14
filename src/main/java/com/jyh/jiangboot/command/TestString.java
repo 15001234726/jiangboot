365a14d8f553b914c5014d3e8cc19a48c049f1f3
jiangyuhong <jyh.mm@qq.com>
2019-05-14 21:51:56 +0800

    字符串是否含有指定字符

--- src/main/java/com/jyh/jiangboot/command/TestString.java
+++ src/main/java/com/jyh/jiangboot/command/TestString.java
@@ -3,10 +3,6 @@ package com.jyh.jiangboot.command;
 public class TestString {
 
 
-
-
-
-
     public static void main(String[] args) {
 
         String aaa = "淘鲜达啦啦啦啦略略略略";
@@ -17,6 +13,21 @@ public class TestString {
         }else{
             System.out.println("不包含");
         }
+
+        int total = 1820;
+        int group = 70;
+        int a = total / group ;
+        int b = total % group;
+        System.out.println(a);
+        System.out.println(b);
+        if (total % group != 0) {
+            System.out.println("1");
+        }
+
+
+//        SingleDemo.getIntstance();
+//        SingleDemo.show();
+
 
 
     }
