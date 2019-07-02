edf290e901155cfe224d41a6ef8a2f2712bca445
jiangyuhong <jyh.mm@qq.com>
2019-07-02 13:58:31 +0800

    ...

---
+++ src/main/java/com/jyh/jiangboot/leetcode/Demo1.java
@@ -0,0 +1,35 @@
+package com.jyh.jiangboot.leetcode;
+
+public class Demo1 {
+
+
+    /**
+     *  无序数列0 到 n  n个数 每个数只出现一次 找到少的那个数
+     *
+     * @param demo
+     * @return
+     */
+
+    public static int test(int[] demo) {
+
+        int result = 0;
+        for (int i = 0; i < demo.length; i ++) {
+            result = result ^ demo[i] ^ i;
+        }
+        result = result ^ (demo.length);
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        int[] demo = new int[] {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14};
+
+        int result = test(demo);
+        System.out.println(result);
+
+
+
+    }
+
+
+}
